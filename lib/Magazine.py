31b8d47b5a6ad9d1d79ebea1ebc5481b2b9ae224
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2023-08-20 17:31:04 +0300

    add contributor method

--- lib/Magazine.py
+++ lib/Magazine.py
@@ -1,11 +1,19 @@
 class Magazine:
     _all = []
+    
     def __init__(self, name, category):
         self.name = name
         self.category = category
         Magazine._all.append(self)
+        self._contributors = []
 
     @classmethod    
     def all(cls):
         return cls._all
+    
+    def add_contributors(self, author):
+        self._contributors.append(author)
+
+    def contributors(self):
+        return self._contributors    
             
