1dbf57d9d8ab3fc8c8ba75561ca330368a6fbc37
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2023-08-20 21:13:22 +0300

    update changes

--- lib/Author.py
+++ lib/Author.py
@@ -1,7 +1,17 @@
+from Article import Article
+
 class Author:
     def __init__(self, name):
         self._name = name
+        self._articles = []
 
     def name(self):
         return self._name
-        
+    
+    def articles(self):
+        return self._articles
+    
+    def magazines(self):
+        return list(set(article._magazine() for article in self._articles))
+    
+   
